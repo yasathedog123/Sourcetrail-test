f17b32bf27d89d226f80d247e94427f2daa73f04
Peter Most <pmost@pera-software.com>
2024-03-06 14:04:47 +0100

    build: Simplify Catch2 version handling

--- src/test/Catch2.hpp
+++ src/test/Catch2.hpp
@@ -2,11 +2,8 @@
 #define CATCH2_HPP
 
 // See https://github.com/catchorg/Catch2/blob/devel/docs/migrate-v2-to-v3.md
-// for the rational behind these header checks.
 
-#if __has_include(<catch2/catch_all.hpp>) // V3?
-	#include <catch2/catch_all.hpp>
-#elif __has_include(<catch2/catch.hpp>) // V2?
+#if defined(HAVE_CATCH2_V2)
 	// Prevent Catch2 from intercepting JVM signals:
 	// (https://github.com/catchorg/Catch2/blob/devel/docs/configuration.md#other-toggles)
 	#define CATCH_CONFIG_NO_WINDOWS_SEH
@@ -14,22 +11,22 @@
 
 	#define CATCH_CONFIG_EXTERNAL_INTERFACES
 	#include <catch2/catch.hpp>
-#else
-	#error "Unknown Catch2 version!"
-#endif
 
-#if CATCH_VERSION_MAJOR == 2
 	namespace Catch2 {
 		using Catch::Detail::Approx;
 
 		using EventListenerBase = Catch::TestEventListenerBase;
 	}
-#elif CATCH_VERSION_MAJOR >= 3
+#elif defined(HAVE_CATCH2_V3)
+	#include <catch2/catch_all.hpp>
+
 	namespace Catch2 {
 		using Catch::Approx;
 
 		using Catch::EventListenerBase;
 	}
+#else
+	#error Unknown Catch2 version!
 #endif
 
 #define REQUIRE_MESSAGE(msg, cond) \
