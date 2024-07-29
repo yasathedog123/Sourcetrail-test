1eb1d7b4b93d7685929c56e41b606ea872e11418
Peter Most <pmost@pera-software.com>
2024-07-29 18:11:38 +0200

    build: Try to reduce vcpkg rebuilds

--- vcpkg-custom-tripplets/custom-x64-linux-static-md.cmake
+++ vcpkg-custom-tripplets/custom-x64-linux-static-md.cmake
@@ -1,11 +1,10 @@
+# Try to prevent rebuilds (See https://github.com/microsoft/vcpkg/issues/26346):
+set(VCPKG_DISABLE_COMPILER_TRACKING ON)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
-
-set(VCPKG_CMAKE_SYSTEM_NAME Linux)
-
-# Try to prevent rebuilds (See https://github.com/microsoft/vcpkg/issues/26346):
-set(VCPKG_DISABLE_COMPILER_TRACKING ON)
 
 # Prevent Catch2 from intercepting JVM signals:
 # https://github.com/catchorg/Catch2/blob/devel/docs/cmake-integration.md#catch_config_-customization-options-in-cmake
--- vcpkg-custom-tripplets/custom-x64-windows-static-md.cmake
+++ vcpkg-custom-tripplets/custom-x64-windows-static-md.cmake
@@ -1,12 +1,10 @@
+# Try to prevent rebuilds (See https://github.com/microsoft/vcpkg/issues/26346):
+set(VCPKG_DISABLE_COMPILER_TRACKING ON)
+
+unset(VCPKG_CMAKE_SYSTEM_NAME) # This is the default, but make it obvious that an unset system name means Windows.
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
-
-# This is the default, but make it obvious that an unset system name means Windows:
-unset(VCPKG_CMAKE_SYSTEM_NAME)
-
-# Try to prevent rebuilds (See https://github.com/microsoft/vcpkg/issues/26346):
-set(VCPKG_DISABLE_COMPILER_TRACKING ON)
 
 # Prevent Catch2 from intercepting JVM signals:
 # https://github.com/catchorg/Catch2/blob/devel/docs/cmake-integration.md#catch_config_-customization-options-in-cmake
