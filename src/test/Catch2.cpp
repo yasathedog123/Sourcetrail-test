171526896889a5e187ec0620b58abc66369f93a3
Peter Most <pmost@pera-software.com>
2024-08-06 16:19:26 +0200

    build: Use single vcpkg triplet file

--- src/test/Catch2.cpp
+++ src/test/Catch2.cpp
@@ -1,16 +1,16 @@
-/*
 #include "Catch2.hpp"
 #include <boost/predef.h>
 
 // Check that the correct switches are defined, so Catch2 is not intercepting JVM signals:
 
-#if BOOST_OS_WINDOWS
-	#if !defined(CATCH_CONFIG_NO_WINDOWS_SEH)
-		#error CATCH_CONFIG_NO_WINDOWS_SEH is not defined!
-	#endif
-#else
-	#if !defined(CATCH_CONFIG_NO_POSIX_SIGNALS)
-		#error CATCH_CONFIG_NO_POSIX_SIGNALS is not defined!
+#if IS_VCPKG_BUILD
+	#if BOOST_OS_LINUX
+		#if !defined(CATCH_CONFIG_NO_POSIX_SIGNALS)
+			#error CATCH_CONFIG_NO_POSIX_SIGNALS is not defined!
+		#endif
+	#elif BOOST_OS_WINDOWS
+		#if !defined(CATCH_CONFIG_NO_WINDOWS_SEH)
+			#error CATCH_CONFIG_NO_WINDOWS_SEH is not defined!
+		#endif
 	#endif
 #endif
-*/
