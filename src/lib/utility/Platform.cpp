122aaaaa21da262516aaed5247023f3390ff59b6
Peter Most <pmost@pera-software.com>
2024-09-23 21:00:04 +0200

    src: Fix MSVC compile error in debug build

--- src/lib/utility/Platform.cpp
+++ src/lib/utility/Platform.cpp
@@ -8,8 +8,8 @@ using namespace std::string_literals;
 static_assert(BOOST_ARCH_X86_32 || BOOST_ARCH_X86_64, "Unknown architecture!");
 static_assert(BOOST_OS_WINDOWS || BOOST_OS_MACOS || BOOST_OS_LINUX, "Unknown operating system!");
 
-static constexpr string X86_32_NAME("32 Bit");
-static constexpr string X86_64_NAME("64 Bit");
+static constexpr char X86_32_NAME[] = "32 Bit";
+static constexpr char X86_64_NAME[] = "64 Bit";
 
 string Platform::getName()
 {
